1aa2dd03eefe562bb6112b7fd26ab3827c7511d2
Mark Mcloughlin <mark25mky@gmail.com>
2024-04-07 17:12:52 +0000

    added some js to only show initial question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("connected")
+// Get all the questions
+const questions = document.querySelectorAll('.question');
+let currentQuestionIndex = 0; // Start with the first question
+
+// Function to show the current question and hide the rest
+function showQuestion() {
+    // Hide all questions
+    questions.forEach((question, index) => {
+        if (index === currentQuestionIndex) {
+            // Show the current question
+            question.style.display = 'block';
+        } else {
+            // Hide other questions
+            question.style.display = 'none';
+        }
+    });
+}
+
+// Initially show the first question
+showQuestion();
